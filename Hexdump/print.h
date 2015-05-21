403e007d469bca998b74a1b8cd347e2581adf8bd
Luan Nguyen <luandnguyen1994@yahoo.com>
2015-05-21 13:55:15 -0700

    Update print.h

--- Hexdump/print.h
+++ Hexdump/print.h
@@ -1,1 +1,23 @@
+/***************************************************************
+Name:         Luan Nguyen
+username:     masc0638
+Class:        CS530, Spring 2015
+Project:      Assignment 1
+File:         print.h
+Note:         Header file of functions for printing hex dump
+***************************************************************/
 
+#ifndef PRINT_H
+#define PRINT_H
+
+#include <iostream>
+#include <iomanip>
+
+const int HEX_LENGTH = 16;
+const int BIN_LENGTH = 6;
+const int ADDRESS_L = 7;
+
+void printHex(const char* hexBuf, unsigned long &address, int &charCount);
+void printBin(const char* binBuf, unsigned long &address, int &charCount);
+
+#endif
